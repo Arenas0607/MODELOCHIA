cf33460de80fa0823b32582a70f2b8bda35cf77e
Arenas0607 <cristian100128@gmail.com>
2025-05-07 18:02:10 -0500

    Corregido: se asegura que el objeto app existe en app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+modelo = joblib.load('modelo_trafico.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    hora = int(request.form['hora'])
+    dia_semana = int(request.form['dia_semana'])
+    prediccion = modelo.predict(np.array([[hora, dia_semana]]))[0]
+    return render_template('index.html', resultado=round(prediccion, 2))
+
+if __name__ == '__main__':
+    app.run(debug=True)

aeccc5176f0aff3daf48a1b6773694f27b6c5386
Arenas0607 <cristian100128@gmail.com>
2025-05-07 19:28:05 -0500

    mejora en pagina principalcorreccion de error

--- app.py
+++ app.py
@@ -3,19 +3,23 @@ import joblib
 import numpy as np
 
 app = Flask(__name__)
-modelo = joblib.load('modelo_trafico.pkl')
+
+modelo = joblib.load("modelo_trafico.pkl")
+le_dia = joblib.load("encoder_dia.pkl")
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', resultado=None)
 
 @app.route('/predecir', methods=['POST'])
 def predecir():
+    
     hora = int(request.form['hora'])
-    dia_semana = int(request.form['dia_semana'])
-    prediccion = modelo.predict(np.array([[hora, dia_semana]]))[0]
-    return render_template('index.html', resultado=round(prediccion, 2))
-
+    dia_semana = request.form['dia_semana']  # verificar funcion genera 550 e
+    dia_codificado = le_dia.transform([dia_semana])[0]
+    prediccion = modelo.predict([[hora, dia_codificado]])
+    return render_template('index.html', resultado=round(prediccion[0], 2))
 if __name__ == '__main__':
     app.run(debug=True)
+
 #verificar modelo 
